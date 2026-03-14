408e1fdb64f09d8defc2735d20bd75f4c244afb6
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-03-14 15:21:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-HF_API_KEY = os.getenv("HF_API_KEY", "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")
+HF_API_KEY = os.getenv("HF_API_KEY", "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")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY", "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")
